3a94d42a9825a10b7f3bb937852833de6d194e0d
daryaWork <69673702+daryaWork@users.noreply.github.com>
2025-06-04 10:09:38 +0300

    add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+    <slot>Начать игру</slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: var(--bg-button);
+  font-size: 24px;
+  border-radius: 100px;
+  padding: 20px 50px;
+  color: var(--color-white);
+  font-weight: 400;
+}
+</style>
